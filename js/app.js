1fe1c1f497670182ab7788f91395f18a67fac5fd
Mélodie MAUBOIS <maubois.melodie@orange.fr>
2018-01-06 20:30:01 +0100

    appli marche sans filtre par départements

--- js/app.js
+++ js/app.js
@@ -5,19 +5,42 @@ function recupCollaborateurs(event){
 	event.preventDefault();
 	
 	$.getJSON(URL + "/collaborateurs").then(result => {
-			
+		
 			var collabs = "";
 			result.forEach((c) => {
-				collabs += "<tr>"; 
+				
+				collabs += "<tr onclick=\"preChargeInputs(event)\">"; 
 				for(variable in c){
-					if(variable == "matricule" || variable == "nom" || variable == "prenom"){
-					collabs += "<td>"+c[variable]+"</td>"}; 
+					
+					if(variable == "matricule" || variable == "nom" || variable == "prenom")
+					{
+						collabs += "<td>"+c[variable]+"</td>"
+					}; 	
 				}
-				collabs += "</tr>";
 			});
-		
-		document.getElementsByTagName("tbody").innerHTML = collabs;
+			
+		document.getElementById("tabBody").innerHTML = collabs;
+	}, erreur => {
+		document.getElementById("tab").innerHTML = "<div class=\"alert alert-danger\">Erreur de communication avec l'API backend</div>";
 	});
+}
+
+function preChargeInputs(event){
+	
+	event.preventDefault();
+	
+	document.getElementById("champsCache").innerHTML = event.path[1].firstChild.innerHTML;
+	
+	$.getJSON(URL + "/collaborateurs/"+event.path[1].firstChild.innerHTML+"/banque").then(result => {
+		
+		document.getElementById("banque").value = result.banque;
+		document.getElementById("bic").value = result.bic;
+		document.getElementById("iban").value = result.iban;
+		
+	}, erreur => {
+		document.getElementById("tab").innerHTML = "<div class=\"alert alert-danger\">Erreur de communication avec l'API backend</div>";
+	});
+
 }
 
 function valider(event){
@@ -27,6 +50,18 @@ function valider(event){
 	var banque = document.getElementById("banque").value;
 	var bic = document.getElementById("bic").value;
 	var iban = document.getElementById("iban").value;
-	
-	$.getJSON(URL + "/collaborateurs/")
+	var collab = {banque: banque, bic: bic, iban: iban};
+
+	$.ajax({
+	    url: URL+"/collaborateurs/"+document.getElementById("champsCache").innerHTML+"/banque",
+	    method: 'PUT',
+	    data: JSON.stringify(collab),
+	    contentType: 'application/json',
+	    success: function(result) {
+	    	console.log(result);
+	    },
+	    error: function(request,msg,error) {
+	    	document.getElementById("champsCache").innerHTML = "<div class=\"alert alert-danger\">Erreur de communication avec l'API backend</div>";
+	    }
+	});
 }
